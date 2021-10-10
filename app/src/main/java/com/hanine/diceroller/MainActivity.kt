aedf69af87444c5efcd63287868fba2f2c63835c
Hanine Benslimene <haninebenslimene@gmail.com>
2021-10-10 13:27:06 +0100

    second commit

--- app/src/main/java/com/hanine/diceroller/MainActivity.kt
+++ app/src/main/java/com/hanine/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.hanine.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +10,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener{
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
+        rollButton.setOnClickListener {
+            // val resultTextView: TextView = findViewById(R.id.textView)
+            rollDice()
+
 
         }
 
@@ -22,5 +23,20 @@ class MainActivity : AppCompatActivity() {
             1 -> diceImage
         }*/
 
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+
+
+    }
+}
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
